4775f5973bd12eebff8ec207e6f32057a4505ebb
buvaneshwaran174-sudo <buvaneshwaran174@gmail.com>
2026-06-30 21:11:07 +0530

    initial commit

---
+++ Addition.java
@@ -0,0 +1,8 @@
+class Addition{
+    public static void main(String args[]){
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println("The sum of a and b is: " + c);
+    }
+}
