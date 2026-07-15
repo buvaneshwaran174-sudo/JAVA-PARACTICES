ba1f450d785a13d63f4b386b04229dc8965a0e56
buvaneshwaran174-sudo <buvaneshwaran174@gmail.com>
2026-07-15 17:18:26 +0530

    JAVA PARACTICES

--- DAY 21/count-vowel-strings-in-range.java
+++ DAY 21/count-vowel-strings-in-range.java
@@ -1,34 +1,17 @@
 class Solution {
-public:
-    int isVowel(char ch){
-        string str = "aeiou";
-        return (str.find(ch) != string::npos);
-    }   
-    vector<int> vowelStrings(vector<string>& A, vector<vector<int>>& Q) {
-        int n = A.size();
-        vector<int> Pre(n,0);
-        for(int i=0;i<n;i++){
-            string temp = A[i];
-            if(isVowel(temp[0]) && isVowel(temp[temp.size()-1])){
-                Pre[i]++;
+    public boolean checkSubarraySum(int[] nums, int k) {
+        Set<Integer> modSet=new HashSet();
+        int currSum = 0, prevSum=0; 
+		//when we add prevSum=0 in set it will actually check if currSum is divided by k 
+        for(int n : nums) {
+            currSum += n;
+            if(modSet.contains(currSum%k)) {
+                return true;
             }
-            // cout << Pre[]
+            currSum %=k;
+            modSet.add(prevSum);
+            prevSum = currSum;
         }
-        for(int i=1;i<n;i++){
-            Pre[i] += Pre[i-1];
-        }
-        vector<int> ans;
-        for(auto it : Q){
-            int l = it[0];
-            int r = it[1];
-            if(l==0){ 
-                ans.push_back(Pre[r]);
-            }else{
-                ans.push_back(Pre[r] - Pre[l-1]);
-            }
-        }
-        return ans;
-
-
+        return false;
     }
-};
+}
